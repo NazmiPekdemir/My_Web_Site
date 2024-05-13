4b8732272448a7f96b18e04ff59fe5b32f82fa0c
Nazmi Pekdemir <pekdemirnazmi@gmail.com>
2024-05-13 11:48:15 +0300

    Css ayarları %5

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+export default function Header() {
+  return (
+    <>
+      <div>
+        <div>
+          <div>Light-Mode / Language</div>
+        </div>
+
+        <div>
+          <p>Lorem</p>
+          <p>lorem 1</p>
+          <p>lorem 2</p>
+          <p>lorem 3</p>
+        </div>
+        <div>
+          <img></img>
+        </div>
+      </div>
+    </>
+  );
+}
